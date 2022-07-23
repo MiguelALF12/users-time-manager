75060ff3e3da03c9246b3405e1878d2c1260d820
Migueldylo <flepard3612@gmail.com>
2022-07-23 11:14:06 -0500

    setting up project

---
+++ FRONTEND/views/placements.py
@@ -0,0 +1,12 @@
+# const PLACEHOLDERS
+placeholders = {
+    'PH_SEARCH_RECORD': "Buscar registro",
+    'PH_HOUR': "00:00",
+    'PH_BRACELET_NUMBER': "0",
+    'PH_TOTAL_USERS': "00",
+    'PH_TOTAL_MONEY': "$0",
+    'PH_NAME': "Ej: Miguel Lopez",
+    'PH_PARENT': "Ej: Juliana Vélez",
+    'PH_PARENT_ID': "Ej: La cédula",
+    'PH_EXTRA_TIME': "Ej: [15,120]"
+}
